b50605b9f35f70490ccf913f97ce4caf28beffb5
IcedComputer <31418197+IcedComputer@users.noreply.github.com>
2019-06-17 16:37:19 -0700

    new
    test this out

---
+++ ovpn12.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+##  Deployment Script for Azure Pihole + VPN service using Cloudflare as DNS service
+##	Created by: Iced Computer
+##  Last Modified 14 June 2019
+## Some info taken from Pivpn & Pihole (launchers)
+##
+
+#What account?
+read -p 'Which Client? ' USER
+
+## VARS
+HOME=~/ovpns
+CLIENTDIR=$HOME/$USER
+CCERT=$CLIENTDIR/${USER}.crt
+CA=/etc/openvpn/easy-rsa/pki/ca.crt
+CKEY=/etc/openvpn/easy-rsa/pki/${USER}.key
+OVPN=$HOME/${USER}.ovpn
+
+
+
+
+## Setup
+mkdir $CLIENTDIR
+mv $OVPN $CLIENTDIR/
+cd $CLIENTDIR
+
+#split OVPN file into components needed
+cat $OVPN|awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".temp"}'
+
+sed '1,2d' $CLIENTDIR/cert1.temp > $CCERT
+
+#clean up
+rm $CLIENTDIR/cert.temp
+rm $CLIENTDIR/cert1.temp
+rm $CLIENTDIR/cert2.temp
+
+openssl pkcs12 -export -in $CCERT -inkey $CKEY -certfile $CA -name $USER -out /$CLIENTDIR/${USER}.ovpn12
+
