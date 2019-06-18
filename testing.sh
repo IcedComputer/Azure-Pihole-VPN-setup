ea182bb63871d878d30bfbe02d21ab80be9e5d6d
IcedComputer <31418197+IcedComputer@users.noreply.github.com>
2019-06-17 17:36:09 -0700

    finally?
    ugh

--- testing.sh
+++ testing.sh
@@ -712,7 +712,7 @@ setClientDNS() {
 
 #This procedure allows a user to specify a custom search domain if they have one.
 setCustomDomain() {
-  DomainSettingsCorrect=False
+  DomainSettingsCorrect=True
 
   if (whiptail --backtitle "Custom Search Domain" --title "Custom Search Domain" --yesno "Would you like to add a custom search domain? \n (This is only for advanced users who have their own domain)\n" ${r} ${c}); then
 
@@ -748,7 +748,7 @@ confOpenVPN() {
     # Generate a random, alphanumeric identifier of 16 characters for this server so that we can use verify-x509-name later that is unique for this server installation. Source: Earthgecko (https://gist.github.com/earthgecko/3089509)
     
 	##ADDED
-	SNAME=echo $(echo hostname)
+	SNAME=$(hostname)
 	echo ${SNAME} > /tmp/INSTALL_Server_Name
     $SUDO cp /tmp/INSTALL_Server_Name /etc/pivpn/INSTALL_Server_Name
 	NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
